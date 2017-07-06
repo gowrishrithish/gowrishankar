247c92b54799d70d0bc541e16d7ca7097d6f9d63
gowrishrithish <gowrishrithish@gmail.com>
2017-07-06 12:16:49 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class palindrome {
+	private static Scanner scan;
+
+	public static void main(String args[])
+    {
+        int num, rem, orig, rev=0;
+        scan = new Scanner(System.in);
+		
+        System.out.print("Enter a Number : ");
+        num = scan.nextInt();
+		
+        orig = num;
+		
+        while(num != 0)
+        {
+            rem = num%10;
+            rev = rev*10 + rem;
+            num = num/10;
+        }
+        
+        // check if the original number is equal to its reverse
+        
+        if(rev==orig)
+        {
+            System.out.print("This is a Palindrome Number");
+        }
+        else
+        {
+            System.out.print("This is not a Palindrome Number");
+        }
+    }
+
+}
