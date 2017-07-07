5e2268f7df2043d135e99216ffcaeddf49cb6aef
gowrishrithish <gowrishrithish@gmail.com>
2017-07-07 14:19:53 +0530

    Add files via upload

---
+++ sumofN.java
@@ -0,0 +1,23 @@
+
+public class sumofN {
+	public static void print_sums(){
+
+    int sum = 0 ;
+     for(int i = 1 ; i <= 5 ; i++){
+         for(int j = 1 ; j <= i; j++ ){
+            sum = sum + j ;
+         }
+         System.out.println( sum) ;
+         sum = 0 ;
+     }
+
+}
+
+public static void main(String[] args)
+{
+    print_sums() ;
+
+
+}
+
+}
