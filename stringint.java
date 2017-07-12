e2d8dfbe1d8fd6e33aa66f7d712104d2a0ed80f3
gowrishrithish <gowrishrithish@gmail.com>
2017-07-12 10:01:55 +0530

    Add files via upload

---
+++ stringint.java
@@ -0,0 +1,10 @@
+
+public class stringint {
+		public static void main(String args[]){  
+		String s="500";  
+		int i=Integer.parseInt(s);  
+		System.out.println(s+200);  
+		System.out.println(i+200); 
+		}
+}
+
