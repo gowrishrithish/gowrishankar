7fa23f01a7291ebeb45e96361220092697ee877e
gowrishrithish <gowrishrithish@gmail.com>
2017-07-13 11:18:52 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,16 @@
+
+public class odd 
+	{
+	public static void main(String org[])
+	{
+	int i;
+	for(i=1;i<=100;i++){//loop for take 1 to 100 numbers
+	if(i%2!=0)//condition to find odd numbers between 1 to 100
+	{
+	System.out.println(i);
+	}
+	}
+	}
+	}
+
+
